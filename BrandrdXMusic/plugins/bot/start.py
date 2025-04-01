2889495f242af566a0801c16d4fdacef2dc3ee3f
Suhaa <154858045+EhSuhaa@users.noreply.github.com>
2025-04-01 08:48:12 +0530

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -100,13 +100,13 @@ async def start_pm(client, message: Message, _):
                     message.chat.photo.big_file_id,
                 )
             else:
-                userss_photo = "assets/nodp.png"
+                userss_photo = "assets/nodp.jpg"
             if userss_photo:
                 chat_photo = userss_photo
             chat_photo = userss_photo if userss_photo else START_IMG_URL
 
         except AttributeError:
-            chat_photo = "assets/nodp.png"
+            chat_photo = "assets/nodp.jpg"
 
     
         await message.reply_photo(

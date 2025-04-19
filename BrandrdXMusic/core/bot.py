a01cd96cbf2fd0976050b7869885abff835ab03a
Suhaa <154858045+EhSuhaa@users.noreply.github.com>
2025-04-19 12:13:16 +0530

    Update bot.py

--- BrandrdXMusic/core/bot.py
+++ BrandrdXMusic/core/bot.py
@@ -28,7 +28,7 @@ class Hotty(Client):
         try:
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=f"<b>Eri Music</b> started successfully </>",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
